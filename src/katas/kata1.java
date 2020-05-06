fa2d3a6179a4cb9baaf1d657aba0632932829597
hhaslam11 <kalebmarc@hotmail.com>
2020-05-06 16:33:46 -0700

    change order of methods in kata1

--- src/katas/kata1.java
+++ src/katas/kata1.java
@@ -5,16 +5,6 @@ import java.util.Arrays;
 import static java.lang.System.out;
 
 public class kata1 {
-  static void test() {
-    int[] testData1 = {1, 10};
-    int[] testData2 = {1, 2, 3};
-    int[] testData3 = {10, 4, 34, 6, 92, 2};
-
-    out.println(run(testData1) + " should be 11");
-    out.println(run(testData2) + " should be 5");
-    out.println(run(testData3) + " should be 126");
-  }
-
   /**
    * Find the two largest numbers in an array, and sum them.
    */
@@ -23,5 +13,15 @@ public class kata1 {
 
     Arrays.sort(numbers);
     return (numbers[len - 1] + numbers[len - 2]);
+  }
+
+  static void test() {
+    int[] testData1 = {1, 10};
+    int[] testData2 = {1, 2, 3};
+    int[] testData3 = {10, 4, 34, 6, 92, 2};
+
+    out.println(run(testData1) + " should be 11");
+    out.println(run(testData2) + " should be 5");
+    out.println(run(testData3) + " should be 126");
   }
 }
