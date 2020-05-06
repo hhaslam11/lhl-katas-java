7f18637ef2bc5c6d7fcfecf1f2df1301bd5b5266
hhaslam11 <kalebmarc@hotmail.com>
2020-05-06 15:58:28 -0700

    change structure a bit

---
+++ src/katas/kata1.java
@@ -0,0 +1,5 @@
+package katas;
+
+public class kata1 {
+
+}
---
+++ src/katas/main.java
@@ -0,0 +1,8 @@
+package katas;
+
+public class main {
+  public static void main(String args[]) {
+    System.out.println("Hello, World! Let's see if I still remember how to do this");
+
+  }
+}
