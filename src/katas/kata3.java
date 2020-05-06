22575a83fd0d8021d213161e8de58bb0518a3356
hhaslam11 <kalebmarc@hotmail.com>
2020-05-06 16:49:02 -0700

    prep kata3

---
+++ src/katas/kata3.java
@@ -0,0 +1,20 @@
+package katas;
+
+public class kata3 {
+  /**
+   * Kata 3 - Vowels
+   * Count the number of vowels that appear in a given string. Vowels are considered: a, e, i, o, u
+   * @param input input string
+   * @return the number of vowels in the input string
+   * @url https://web.compass.lighthouselabs.ca/activities/780
+   */
+  static int run(String input) {
+
+    return 0;
+  }
+  static void test() {
+    System.out.println(run("orange") + " should be 3");
+    System.out.println(run("lighthouse labs") + " should be 5");
+    System.out.println(run("aeiou") + " should be 5");
+  }
+}
--- src/katas/main.java
+++ src/katas/main.java
@@ -3,7 +3,7 @@ package katas;
 public class main {
   public static void main(String args[]) {
 //    kata1.test();
-    kata2.test();
-
+//    kata2.test();
+    kata3.test();
   }
 }
