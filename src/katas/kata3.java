05df09b28ae32a20bb2bdda97729b010ea8a0955
hhaslam11 <kalebmarc@hotmail.com>
2020-05-06 17:01:55 -0700

    finish kata3

--- src/katas/kata3.java
+++ src/katas/kata3.java
@@ -9,8 +9,20 @@ public class kata3 {
    * @url https://web.compass.lighthouselabs.ca/activities/780
    */
   static int run(String input) {
+    int totalVowels = 0;
 
-    return 0;
+    for (int i = 0; i < input.length(); i++) {
+      switch(input.charAt(i)) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+          totalVowels++;
+      }
+    }
+
+    return totalVowels;
   }
   static void test() {
     System.out.println(run("orange") + " should be 3");
