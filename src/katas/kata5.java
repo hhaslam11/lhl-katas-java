c871f0673838a0be547456ab977e3170410efd26
hhaslam11 <kalebmarc@hotmail.com>
2020-05-06 17:55:27 -0700

    did kata5 without using String.replace()

--- src/katas/kata5.java
+++ src/katas/kata5.java
@@ -4,12 +4,23 @@ public class kata5 {
   /**
    * Kata 5 - Percent Encoded String
    * Given a normal string of words, turn it into a percent-encoded string by replacing all whitespace with %20.
+   * Do NOT use String.replace().
    * @param text text to be encoded
    * @return the string with all spaces replaced with %20
    * @url https://web.compass.lighthouselabs.ca/activities/782
    */
   private static String run(String text) {
-    return text.trim().replaceAll(" ", "%20");
+    text = text.trim();
+    String newString = "";
+
+    for (int i = 0; i < text.length(); i++) {
+      if (text.charAt(i) == ' ') {
+        newString += "%20";
+      } else {
+        newString += text.charAt(i);
+      }
+    }
+    return newString;
   }
   public static void test() {
     System.out.println(run("Lighthouse Labs") + " should be Lighthouse%20Labs");
