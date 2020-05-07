7e3c3053e05d01d2011cb517673017877098b351
hhaslam11 <kalebmarc@hotmail.com>
2020-05-06 17:37:05 -0700

    prep kata4

---
+++ src/katas/kata4.java
@@ -0,0 +1,55 @@
+package katas;
+
+public class kata4 {
+  /**
+   * Kata 4 - Instructors Names
+   * Given an array of Instructors, determine which instructor has the longest name
+   * @param instructors array of Instructors
+   * @return the instructor with the longest name
+   * @url https://web.compass.lighthouselabs.ca/activities/781
+   */
+  static Instructor run(Instructor[] instructors) {
+    return instructors[0];
+  }
+  static void test() {
+    final Instructor samuel   = new Instructor("Samuel", "iOS");
+    final Instructor jeremiah = new Instructor("Jeremiah", "iOS");
+    final Instructor ophilia  = new Instructor("Ophilia", "iOS");
+    final Instructor donald   = new Instructor("Donald", "iOS");
+    final Instructor matthew  = new Instructor("Matthew", "iOS");
+    final Instructor david    = new Instructor("David", "iOS");
+    final Instructor domascus = new Instructor("Domascus", "iOS");
+
+    Instructor[] testData1 = {samuel, jeremiah, ophilia, donald};
+    Instructor[] testData2 = {matthew, david, domascus, donald};
+
+    System.out.println(run(testData1).getName() + " should be Jeremiah");
+    System.out.println(run(testData2).getName() + " should be Domascus");
+  }
+}
+
+class Instructor {
+  private String name;
+  private String course;
+
+  protected Instructor(String name, String course) {
+    this.name = name;
+    this.course = course;
+  }
+
+  protected void setCourse(String course) {
+    this.course = course;
+  }
+
+  protected void setName(String name) {
+    this.name = name;
+  }
+
+  protected String getCourse() {
+    return course;
+  }
+
+  protected String getName() {
+    return name;
+  }
+}
--- src/katas/main.java
+++ src/katas/main.java
@@ -4,6 +4,7 @@ public class main {
   public static void main(String args[]) {
 //    kata1.test();
 //    kata2.test();
-    kata3.test();
+//    kata3.test();
+    kata4.test();
   }
 }
